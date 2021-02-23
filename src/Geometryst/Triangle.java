c8e1d40a13302c1578a0fef0b49ba6a3dd02b544
Tom <tom-vautray@hotmail.fr>
2021-02-23 23:51:13 +0100

    Triangle class

---
+++ src/Geometryst/Triangle.java
@@ -0,0 +1,35 @@
+package Geometryst;
+
+public class Triangle
+{
+	Point _a, _b, _c;
+	
+	Triangle(Point a, Point b, Point c)
+	{
+		_a = a;
+		_b = b;
+		_c = c;
+	}
+	
+	public boolean isoscele()
+	{
+		double
+			ab = _a.distance(_b),
+			bc = _b.distance(_c),
+			ca = _c.distance(_a);
+		
+		if(
+			ab == bc
+			|| bc == ca
+			|| ca == ab
+		)
+			return true;
+		
+		return false;
+	}
+	
+	public String toString()
+	{
+		return "Triangle: a: " + _a + " b: " + _b + " c: " + _c;
+	}
+}
